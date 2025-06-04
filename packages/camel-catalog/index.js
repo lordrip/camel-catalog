422049a24412aca8ee8e608045f1a4893855aec2
mmelko <mmelko@redhat.com>
2025-06-04 14:15:31 +0200

    feat: separate catalog versions into separate index.js and make that exportable

---
+++ packages/camel-catalog/index.js
@@ -0,0 +1,9 @@
+const CATALOGS = {
+  Main: ['4.11.0', '4.10.4', '4.8.6', '4.10.3.redhat-00020', '4.8.5.redhat-00008', '4.4.0.redhat-00046'],
+  Quarkus: ['3.20.0', '3.15.3', '3.20.0.redhat-00002', '3.15.0.redhat-00010', '3.8.0.redhat-00018'],
+  SpringBoot: ['4.11.0', '4.10.4', '4.8.6', '4.10.3.redhat-00019', '4.8.5.redhat-00008', '4.4.0.redhat-00039'],
+};
+
+const KAMELETS_VERSION = '4.11.0';
+
+module.exports = { CATALOGS, KAMELETS_VERSION };
--- packages/camel-catalog/scripts/build-catalogs.mjs
+++ packages/camel-catalog/scripts/build-catalogs.mjs
@@ -8,46 +8,10 @@ import { resolve } from 'path';
 import { existsSync } from 'node:fs';
 
 const require = createRequire(import.meta.url);
-
+const { CATALOGS, KAMELETS_VERSION } = require('../index.js');
 /**
  * @type {Record<import('../dist/types').CatalogRuntime, string[]>}
  **/
-const CATALOGS = {
-  // https://repo1.maven.org/maven2/org/apache/camel/camel-catalog/
-  // https://maven.repository.redhat.com/ga/org/apache/camel/camel-catalog/
-  Main: [
-    //
-    '4.11.0',
-    '4.10.4',
-    '4.8.6',
-    '4.10.3.redhat-00020',
-    '4.8.5.redhat-00008',
-    '4.4.0.redhat-00046',
-  ],
-  // https://repo1.maven.org/maven2/org/apache/camel/quarkus/camel-quarkus-catalog/
-  // https://maven.repository.redhat.com/ga/org/apache/camel/quarkus/camel-quarkus-catalog/
-  Quarkus: [
-    //
-    '3.20.0', // Camel 4.10.2
-    '3.15.3', // Camel 4.8.5
-    '3.20.0.redhat-00002', // Camel 4.10.3.redhat-00020
-    '3.15.0.redhat-00010', // Camel 4.8.0.redhat-00015
-    '3.8.0.redhat-00018', // Camel 4.4.0.redhat-00046
-  ],
-  // https://repo1.maven.org/maven2/org/apache/camel/springboot/camel-catalog-provider-springboot/
-  // https://maven.repository.redhat.com/ga/org/apache/camel/springboot/camel-catalog-provider-springboot/
-  SpringBoot: [
-    //
-    '4.11.0',
-    '4.10.4',
-    '4.8.6',
-    '4.10.3.redhat-00019',
-    '4.8.5.redhat-00008',
-    '4.4.0.redhat-00039',
-  ],
-};
-// https://repo1.maven.org/maven2/org/apache/camel/kamelets/camel-kamelets/
-const KAMELETS_VERSION = '4.11.0';
 
 const generateCatalogs = () => {
   let camelCatalogPath = '';
