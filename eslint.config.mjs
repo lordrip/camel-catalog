3f9f7975d66d72c500437cb6d7dcfaaf65be053b
mmelko <mmelko@redhat.com>
2025-06-19 20:12:21 +0200

    refactor: move catalog to standalone package instead of using monorepo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,16 +6,16 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    rules: {
-      '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-expressions': 'off',
+    { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        rules: {
+            '@typescript-eslint/no-unused-vars': 'off',
+            '@typescript-eslint/no-unused-expressions': 'off',
+        },
     },
-  },
-  eslintConfigPrettier,
-  eslintPluginPrettier,
+    eslintConfigPrettier,
+    eslintPluginPrettier,
 ];
