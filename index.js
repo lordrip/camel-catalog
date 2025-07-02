e469399e5901772e48517737c7137f9801c0bbda
Aurélien Pupier <apupier@redhat.com>
2025-07-02 16:43:38 +0200

    Provide guidance to find the catalog version
    retrieved from
    https://github.com/KaotoIO/kaoto/pull/2320/files#diff-ed02fb209b71484c10f8ebb3f1352ae6cfd125885577738a74b31670a7c15767
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const CATALOGS = {
+  // https://repo1.maven.org/maven2/org/apache/camel/camel-catalog/
+  // https://maven.repository.redhat.com/ga/org/apache/camel/camel-catalog/
   Main: ['4.12.0', '4.10.4', '4.8.6', '4.10.3.redhat-00020', '4.8.5.redhat-00008', '4.4.0.redhat-00046'],
+  // https://repo1.maven.org/maven2/org/apache/camel/quarkus/camel-quarkus-catalog/
+  // https://maven.repository.redhat.com/ga/org/apache/camel/quarkus/camel-quarkus-catalog/
   Quarkus: ['3.24.0', '3.20.2', '3.15.3', '3.20.0.redhat-00002', '3.15.0.redhat-00010', '3.8.0.redhat-00018'],
+  // https://repo1.maven.org/maven2/org/apache/camel/springboot/camel-catalog-provider-springboot/
+  // https://maven.repository.redhat.com/ga/org/apache/camel/springboot/camel-catalog-provider-springboot/
   SpringBoot: ['4.12.0', '4.10.4', '4.8.6', '4.10.3.redhat-00019', '4.8.5.redhat-00008', '4.4.0.redhat-00039'],
 };
 
