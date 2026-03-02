01df25bfa3cbcbc6b5e7ac63e73848293c98afaf
Ricardo M. <lordrip@gmail.com>
2026-03-02 09:44:44 +0100

    feat(Catalogs): Update latest LTS camel versions

--- index.js
+++ index.js
@@ -17,17 +17,31 @@
 const CATALOGS = {
   // https://repo1.maven.org/maven2/org/apache/camel/camel-catalog/
   // https://maven.repository.redhat.com/ga/org/apache/camel/camel-catalog/
-  Main: ['4.17.0', '4.14.4', '4.14.2.redhat-00011', '4.10.7.redhat-00009', '4.8.5.redhat-00008', '4.4.0.redhat-00046'],
+  Main: ['4.18.0', '4.14.5', '4.14.2.redhat-00019', '4.10.7.redhat-00009', '4.8.5.redhat-00008', '4.4.0.redhat-00046'],
   // https://repo1.maven.org/maven2/org/apache/camel/quarkus/camel-quarkus-catalog/
   // https://maven.repository.redhat.com/ga/org/apache/camel/quarkus/camel-quarkus-catalog/
-  Quarkus: ['3.30.0', '3.27.2', '3.27.1.redhat-00003', '3.20.0.redhat-00010', '3.15.0.redhat-00010', '3.8.0.redhat-00018'],
+  Quarkus: [
+    '3.32.0',
+    '3.27.2',
+    '3.27.1.redhat-00004',
+    '3.20.0.redhat-00010',
+    '3.15.0.redhat-00010',
+    '3.8.0.redhat-00018',
+  ],
   // https://repo1.maven.org/maven2/org/apache/camel/springboot/camel-catalog-provider-springboot/
   // https://maven.repository.redhat.com/ga/org/apache/camel/springboot/camel-catalog-provider-springboot/
-  SpringBoot: ['4.17.0', '4.14.4', '4.14.2.redhat-00015', '4.10.7.redhat-00013', '4.8.5.redhat-00008', '4.4.0.redhat-00039'],
+  SpringBoot: [
+    '4.18.0',
+    '4.14.5',
+    '4.14.2.redhat-00018',
+    '4.10.7.redhat-00013',
+    '4.8.5.redhat-00008',
+    '4.4.0.redhat-00039',
+  ],
   // https://repo1.maven.org/maven2/org/citrusframework/citrus-catalog-schema/
   Citrus: ['4.9.2'],
 };
 
-const KAMELETS_VERSION = '4.15.0';
+const KAMELETS_VERSION = '4.18.0';
 
 module.exports = { CATALOGS, KAMELETS_VERSION };
