6a530c68850dc0d1d6fd7414fe5f92a90cb69ea3
Ricardo M. <lordrip@gmail.com>
2026-05-27 13:39:56 +0200

    chore: update camel catalog versions

--- index.js
+++ index.js
@@ -10,38 +10,55 @@
  *
  * For Red Hat versions, add `--repo=redhat` to the commands.
  * - Main: `camel version list --fresh --runtime=main --repo=redhat`
- * - Quarkus: `camel version list --fresh --runtime=quarkus --repo=redhat``
- * - Spring Boot: `camel version list --fresh --runtime=spring-boot --repo=redhat``
+ * - Quarkus: `camel version list --fresh --runtime=quarkus --repo=redhat`
+ * - Spring Boot: `camel version list --fresh --runtime=spring-boot --repo=redhat`
  */
 
 const CATALOGS = {
   // https://repo1.maven.org/maven2/org/apache/camel/camel-catalog/
   // https://maven.repository.redhat.com/ga/org/apache/camel/camel-catalog/
-  Main: ['4.18.0', '4.14.5', '4.14.2.redhat-00019', '4.10.7.redhat-00009', '4.8.5.redhat-00008', '4.4.0.redhat-00046'],
+  Main: [
+    //
+    '4.20.0',
+    '4.18.2',
+    '4.14.7',
+    '4.10.9',
+    '4.18.1.redhat-00019',
+    '4.14.4.redhat-00008',
+    '4.10.7.redhat-00009',
+  ],
   // https://repo1.maven.org/maven2/org/apache/camel/quarkus/camel-quarkus-catalog/
   // https://maven.repository.redhat.com/ga/org/apache/camel/quarkus/camel-quarkus-catalog/
   Quarkus: [
-    '3.32.0',
-    '3.27.2',
-    '3.27.1.redhat-00004',
-    '3.20.0.redhat-00010',
+    //
+    '3.35.0',
+    '3.33.1',
+    '3.27.4',
+    '3.20.4',
+    '3.27.1.redhat-00007',
+    '3.20.0.redhat-00011',
     '3.15.0.redhat-00010',
-    '3.8.0.redhat-00018',
   ],
   // https://repo1.maven.org/maven2/org/apache/camel/springboot/camel-catalog-provider-springboot/
   // https://maven.repository.redhat.com/ga/org/apache/camel/springboot/camel-catalog-provider-springboot/
   SpringBoot: [
-    '4.18.0',
-    '4.14.5',
-    '4.14.2.redhat-00018',
+    //
+    '4.20.0',
+    '4.18.2',
+    '4.14.7',
+    '4.10.9',
+    '4.18.1.redhat-00014',
+    '4.14.4.redhat-00010',
     '4.10.7.redhat-00013',
-    '4.8.5.redhat-00008',
-    '4.4.0.redhat-00039',
   ],
   // https://repo1.maven.org/maven2/org/citrusframework/citrus-catalog-schema/
-  Citrus: ['4.10.0', '4.10.1'],
+  Citrus: [
+    //
+    '4.10.0',
+    '4.10.1',
+  ],
 };
 
-const KAMELETS_VERSION = '4.18.0';
+const KAMELETS_VERSION = '4.20.0';
 
 module.exports = { CATALOGS, KAMELETS_VERSION };
