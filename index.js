98bbc1e983af2b55b7a44ec08d09e2cebf7679ce
Dominik Jelinek <djelinek@redhat.com>
2026-06-09 11:51:22 +0200

    chore(catalog): skip also 4.14.x.redhat Camel Catalog version of Spring Boot and Quarkus
    removed because there is a bug which prevents the version to be used
    with --console flag of Camel CLI / Camel Launcher

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const CATALOGS = {
     '3.27.4',
     '3.20.6.1',
     '3.33.1.redhat-00006',
-    '3.27.3.redhat-00003',
+    // '3.27.3.redhat-00003', // removed because there is a bug which prevents the version to be used with --console flag of Camel CLI / Camel Launcher
     '3.20.6.redhat-00004',
   ],
   SpringBoot: [
@@ -35,7 +35,7 @@ const CATALOGS = {
     '4.14.7',
     '4.10.9',
     '4.18.1.redhat-00014',
-    '4.14.4.redhat-00010',
+    // '4.14.4.redhat-00010', // removed because there is a bug which prevents the version to be used with --console flag of Camel CLI / Camel Launcher
     '4.10.7.redhat-00013',
   ],
   Citrus: ['4.10.0', '4.10.1'],
