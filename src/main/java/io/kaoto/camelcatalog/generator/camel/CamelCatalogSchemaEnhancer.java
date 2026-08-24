a2586afc10c6617be9fdbf663d9e6b09e8cd3b39
Naive_Dev97 <59308013+dhavalrasputala@users.noreply.github.com>
2026-08-24 14:25:30 +0530

    Refactor/rename java type to model name to java type to model name #152 (#170)
    * Rename to match CamelCaseing
    * rename-JavaTypeToModelName-to-javaTyprToModelName

--- src/main/java/io/kaoto/camelcatalog/generator/camel/CamelCatalogSchemaEnhancer.java
+++ src/main/java/io/kaoto/camelcatalog/generator/camel/CamelCatalogSchemaEnhancer.java
@@ -27,8 +27,8 @@ import java.util.*;
 public class CamelCatalogSchemaEnhancer {
 
     private final CamelCatalog camelCatalog;
-    private final Map<String, String> JAVA_TYPE_TO_MODEL_NAME = new HashMap<>();
-    private final Map<String, String> MODEL_NAME_TO_JAVA_TYPE = new HashMap<>();
+    private final Map<String, String> javaTypeToModelName = new HashMap<>();
+    private final Map<String, String> modelNameToJavaType = new HashMap<>();
     ObjectMapper jsonMapper = new ObjectMapper();
 
     public CamelCatalogSchemaEnhancer(CamelCatalog camelCatalog) {
@@ -217,16 +217,16 @@ public class CamelCatalogSchemaEnhancer {
      * @return the Camel model
      */
     public EipModel getCamelModelByJavaType(String javaType) {
-        return camelCatalog.eipModel(JAVA_TYPE_TO_MODEL_NAME.get(javaType));
+        return camelCatalog.eipModel(javaTypeToModelName.get(javaType));
     }
 
     public String getJavaTypeByModelName(String modelName) {
-        String javaType = MODEL_NAME_TO_JAVA_TYPE.get(modelName);
+        String javaType = modelNameToJavaType.get(modelName);
         if (javaType == null) {
             EipModel model = camelCatalog.eipModel(modelName);
             if (model != null) {
                 javaType = model.getJavaType();
-                MODEL_NAME_TO_JAVA_TYPE.put(modelName, javaType);
+                modelNameToJavaType.put(modelName, javaType);
             }
         }
         return javaType;
@@ -352,8 +352,8 @@ public class CamelCatalogSchemaEnhancer {
         camelCatalog.findModelNames().forEach(modelName -> {
             EipModel model = camelCatalog.eipModel(modelName);
             if (model != null) {
-                JAVA_TYPE_TO_MODEL_NAME.put(model.getJavaType(), modelName);
-                MODEL_NAME_TO_JAVA_TYPE.put(modelName, model.getJavaType());
+                javaTypeToModelName.put(model.getJavaType(), modelName);
+                modelNameToJavaType.put(modelName, model.getJavaType());
             }
         });
     }
